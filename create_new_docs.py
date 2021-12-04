88feaf8131bec1738549a256aa6828c348eff9c6
Jay Miller <kjaymiller@gmail.com>
2021-12-04 07:20:34 -0800

    move create_folder logic to function

--- create_new_docs.py
+++ create_new_docs.py
@@ -4,13 +4,15 @@ Docs and references:
   - [Create and populate folders  |  Google Drive API  |  Google Developers](https://developers.google.com/drive/api/v3/folder)
   Query from [Google Drive API in Python | List Files and Folders in a Google Drive Folder - YouTube](https://www.youtube.com/watch?v=kFR-O8BHIH4)
 """
-from auth import docs, files
-import typer
 import os
 from typing import Optional
 
+import typer
+
+from auth import docs, files
+
 folder_mime_type = "application/vnd.google-apps.folder"
-parent_folder = os.environ.get('PARENT_FOLDER_ID')
+parent_folder = os.environ.get("PARENT_FOLDER_ID")
 
 
 def get_ep_number(title):
@@ -27,7 +29,7 @@ def get_last_episode_number() -> list[int]:
     qfolder = f"name contains 'Episode'"
     qtrash = "trashed = false"
     q = "and".join((qmime_type, qparents, qfolder, qtrash))
-    
+
     # create search request
     response = files.files().list(q=q).execute()
 
@@ -36,9 +38,11 @@ def get_last_episode_number() -> list[int]:
     return max(episodes)
 
 
-def create_folder(
-        count: Optional[int] = typer.Option(1, '-c', help="Number of Folders"),
-        replacements: Optional[list[str]] = typer.Option(None, '-r', help="substitution in key:value format"),
+def main(
+    count: Optional[int] = typer.Option(1, "-c", help="Number of Folders"),
+    replacements: Optional[list[str]] = typer.Option(
+        None, "-r", help="substitution in key:value format"
+    ),
 ) -> None:
     """Create Folders with a Planning Doc inside. Use '-r' to replace values inside"""
 
@@ -48,21 +52,13 @@ def create_folder(
     for entry in range(count):
         episode_number = last_episode + entry + 1
 
-        # metadata for folder 
-        file_metadata = {
-            'name': f'Episode {episode_number}',
-            "mimeType": folder_mime_type,
-            "parents": [parent_folder],
-        }
-
-        # create folder
-        file = files.files().create(
-                body=file_metadata,
-                fields='id, name'
-        ).execute()
+        folder_id = create_folder(episode_number, "Episode")
 
         # copy planning doc into created folder
-        planning_doc = create_doc(name=f"Conduit {episode_number} Planning Doc", parent=file['id'])
+        planning_doc = create_doc(
+            name=f"Conduit {episode_number} Planning Doc",
+            parent=folder_id,
+        )
 
         # process substitutions
         requests = [replace_text(f"episode_number:{episode_number}")]
@@ -73,40 +69,56 @@ def create_folder(
 
         if requests:
             docs.documents().batchUpdate(
-                    documentId=planning_doc,
-                    body={'requests': requests},
+                documentId=planning_doc,
+                body={"requests": requests},
             ).execute()
 
-        print(f"{file['name']}: {file['id']}")
 
 
-def create_doc(name:str, parent:str) -> str:
+def create_folder(episode_number: int, folder_name: str):
+    """builds folder object and returns ID"""
+    # metadata for folder
+    file_metadata = {
+        "name": f"{folder_name} {episode_number}",
+        "mimeType": folder_mime_type,
+        "parents": [parent_folder],
+    }
+
+    # create folder
+    file = files.files().create(body=file_metadata, fields="id, name").execute()
+    typer.echo(f"{file['name']}: {file['id']}")
+    return file.get("id")
+
+
+def create_doc(name: str, parent: str) -> str:
     """Create two documents using the templates"""
-    shownotes_id = os.environ.get('COPY_DOC_ID')
+    shownotes_id = os.environ.get("COPY_DOC_ID")
     shownotes_body = {
         "name": name,
         "parents": [parent],
     }
-    drive_response = files.files().copy(
-        fileId=shownotes_id, body=shownotes_body).execute()
-    planning_doc_id = drive_response.get('id')
+    drive_response = (
+        files.files().copy(fileId=shownotes_id, body=shownotes_body).execute()
+    )
+    planning_doc_id = drive_response.get("id")
     print(f"{planning_doc_id=}")
-    return planning_doc_id 
+    return planning_doc_id
 
 
 def replace_text(replacement_pattern: str) -> dict[str, str]:
     """Wrapper for the replace text functionality for batchUpdates"""
     template_var, replacement_text = replacement_pattern.split(":", maxsplit=1)
-    template_var = "{{" +  template_var + "}}"
+    template_var = "{{" + template_var + "}}"
     return {
-            'replaceAllText': {
-                'containsText': {
-                    'text': template_var,
-                    'matchCase': 'true',
-                },
-            'replaceText': replacement_text,
-                }}
+        "replaceAllText": {
+            "containsText": {
+                "text": template_var,
+                "matchCase": "true",
+            },
+            "replaceText": replacement_text,
+        }
+    }
 
 
 if __name__ == "__main__":
-    typer.run(create_folder)
+    typer.run(main)

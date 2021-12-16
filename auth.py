322872ae04c87c40749b69080cd16bd097ad03ef
Jay Miller <kjaymiller@gmail.com>
2021-12-15 21:25:42 -0800

    add config file

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 """Built with the boilerplate of the Google Docs API"""
 
 import os.path
+import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -16,18 +17,20 @@ creds = None
 # The file token.json stores the user's access and refresh tokens, and is
 # created automatically when the authorization flow completes for the first
 # time.
-if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+token_path='token.json'
+if os.path.exists(token_path):
+    creds = Credentials.from_authorized_user_file(token_path, SCOPES)
 # If there are no (valid) credentials available, let the user log in.
 if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
+        os.remove(token_path)
         creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
             'credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
-    with open('token.json', 'w') as token:
+    with open(token_path, 'w') as token:
         token.write(creds.to_json())
 
 docs = build('docs', 'v1', credentials=creds)
--- create_new_docs.py
+++ create_new_docs.py
@@ -4,8 +4,10 @@ Docs and references:
   - [Create and populate folders  |  Google Drive API  |  Google Developers](https://developers.google.com/drive/api/v3/folder)
   Query from [Google Drive API in Python | List Files and Folders in a Google Drive Folder - YouTube](https://www.youtube.com/watch?v=kFR-O8BHIH4)
 """
+import json
 import logging
 import os
+from pathlib import Path
 from typing import Optional
 
 import typer
@@ -15,6 +17,24 @@ from auth import docs, files
 folder_mime_type = "application/vnd.google-apps.folder"
 parent_folder = os.environ.get("PARENT_FOLDER_ID")
 
+
+def load_config(
+        config_files: Optional[list[Path]]=typer.Option(None)
+        ) -> dict[str,str]:
+    """Creates Dictionary of Values from Default Config json file and any passed in config
+    files"""
+    base_config='BASE_CONFIG.json'
+
+    with open(base_config) as base_jfile:
+        config = json.load(base_jfile)
+
+    # iterate through provided config files.
+    for config_file in config_files:
+        # Overwrite Base Config if environment variable exists
+        with open(os.environ.get('POD_CONFIG_FILE', BASE_CONFIG)) as jfile:
+            config.update(json.load(jfile))
+
+    return config
 
 def get_ep_number(title):
     """gets the number from the end of the Episode"""
@@ -44,20 +64,22 @@ def main(
     replacements: Optional[list[str]] = typer.Option(
         None, "-r", help="substitution in key:value format"
     ),
+    config_file: Optional[Path] = typer.Option(None, "-f", help="PATH of CONFIG"),
 ) -> None:
     """Create Folders with a Planning Doc inside. Use '-r' to replace values inside"""
 
-    # get next episode number
+    # get next episode number and load config
     last_episode = get_last_episode_number()
 
     for entry in range(count):
         episode_number = last_episode + entry + 1
-
-        folder_id = create_folder(episode_number, "Episode")
+        folder_id = create_folder(episode_number,
+        from_config['FOLDER_WATCH_TEMPLATE'])
 
         # copy planning doc into created folder
-        planning_doc = create_doc(
+        planning_doc = copy_template_doc(
             name=f"Conduit {episode_number} Planning Doc",
+            template_filename="",
             parent=folder_id,
         )
 
@@ -90,7 +112,9 @@ def create_folder(episode_number: int, folder_name: str):
     return file.get("id")
 
 
-def create_doc(name: str, parent: str) -> str:
+def copy_template_doc(
+        name: str, template_filename: str, parent: str
+        ) -> str:
     """Create two documents using the templates"""
     shownotes_id = os.environ.get("COPY_DOC_ID")
     shownotes_body = {
@@ -121,4 +145,4 @@ def replace_text(replacement_pattern: str) -> dict[str, str]:
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    typer.run(load_config)
